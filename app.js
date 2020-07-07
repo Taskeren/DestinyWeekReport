7e32243f4c2bfeb4568ab0bcb15f28519896f663
Taskeren <nitu2003@126.com>
2020-07-07 15:11:46 +0800

    [feature] 切换周数不再跳转
    重写了周数改变部分的脚本，现在不再跳转，而是直接修改数据。

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 
 const app = {
 
-    week: Math.ceil(new Date().getTime() / (7 * 24 * 60 * 60 * 1000))
+    week: 9999
 
 };
 
-function init() {
-
+/**
+ * 获取周数
+ */
+function updateWeekNumber() {
     //　获取周数（是否设置）
     let search = document.location.search;
     if(search) {
@@ -15,18 +17,54 @@ function init() {
         if(week) { // 确保周数是个数
             app.week = week;
         }
+        else {
+            app.week = Math.ceil(new Date().getTime() / (7 * 24 * 60 * 60 * 1000));
+        }
+    }
+    else {
+        app.week = Math.ceil(new Date().getTime() / (7 * 24 * 60 * 60 * 1000));
     }
 
+    console.log("Automatic-Week", app.week);
+}
+
+/**
+ * 重算周数，用于跳转回本周
+ */
+function resetWeekNumber() {
+    return app.week = Math.ceil(new Date().getTime() / (7 * 24 * 60 * 60 * 1000));
+}
+
+/**
+ * 更新组件
+ */
+function updateComponents() {
     // 显示周数
     document.querySelector("#week").innerHTML = app.week;
 
     // 设置+1周按钮
-    document.querySelector(".week-add").href = "?"+(app.week+1);
+    document.querySelector(".week-add").onclick = ()=>{
+        changeWeek(app.week+1);
+    };
 
     // 更新所有<challenge>元素
     rotate.updateAll();
 
     // 更新所有<seasonal>元素
     seasonal.updateAll();
+}
 
+function init() {
+    updateWeekNumber();
+    updateComponents();
+}
+
+/**
+ * 更换周数
+ */
+function changeWeek(week) {
+    console.log("Set Week", week);
+    app.week = week;
+    history.pushState(null, "", "?"+week);
+    updateComponents();
 }
